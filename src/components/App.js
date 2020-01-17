d06bd1d4e49abdb0b78664b328d1dabdb4a04190
vieiramanda11 <amanda_vieira@outlook.com>
2020-01-17 14:27:41 -0300

    implementing display component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import Display from './Display';
-import ButtonPanel from './ButtonPanel';
+// import ButtonPanel from './ButtonPanel';
 
 function App() {
   return (
     <div className="App">
       <Display />
-      <ButtonPanel />
     </div>
   );
 }
