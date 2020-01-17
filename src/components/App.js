c46f01315bb572b7bd11d255dfc51cde11d4507f
vieiramanda11 <amanda_vieira@outlook.com>
2020-01-17 14:40:18 -0300

    implementing buttonpannel

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Display from './Display';
-// import ButtonPanel from './ButtonPanel';
+import ButtonPanel from './ButtonPanel';
 
 function App() {
   return (
     <div className="App">
       <Display />
+      <ButtonPanel />
     </div>
   );
 }
