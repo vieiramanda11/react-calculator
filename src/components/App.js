04d442e944dfc5513ef2663e125e2ddf70720e9a
vieiramanda11 <amanda_vieira@outlook.com>
2020-01-20 12:24:15 -0300

    refactoring class component and adding its states

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 
-function App() {
-  return (
-    <div className="app">
-      <Display />
-      <ButtonPanel />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <Display />
+        <ButtonPanel />
+      </div>
+    );
+  }
 }
 
 export default App;
